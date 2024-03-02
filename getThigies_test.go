04d8328547e7f6c560ed87e6bba7d575b678cbc7
Xavi Aznar <xavier.aznar@seat.es>
2024-03-02 11:45:09 +0000

    refactor: based on Dave Cheney article https://dave.cheney.net/2019/05/07/prefer-table-driven-tests

--- getThigies_test.go
+++ getThigies_test.go
@@ -8,30 +8,32 @@ import (
 
 func Test_getThingies(t *testing.T) {
 	r := setupRouter()
-	type testCase struct {
+
+	// Based on https://dave.cheney.net/2019/05/07/prefer-table-driven-tests
+	testCases := map[string]struct {
 		method      string
 		uri         string
 		wantedCode  int
 		wantedError error
-	}
-
-	testCases := []testCase{
-		{method: "GET", uri: "/api/v1/thingy", wantedCode: http.StatusOK, wantedError: nil},
+	}{
+		"get thingy": {method: "GET", uri: "/api/v1/thingy", wantedCode: http.StatusOK, wantedError: nil},
 	}
 
 	w := httptest.NewRecorder()
 
-	for _, tc := range testCases {
-		req, err := http.NewRequest(tc.method, tc.uri, nil)
-		if err != tc.wantedError {
-			t.Fatalf("wanted error %v but got %v", tc.wantedError, err)
-			return
-		}
-		r.ServeHTTP(w, req)
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			req, err := http.NewRequest(tc.method, tc.uri, nil)
+			if err != tc.wantedError {
+				t.Fatalf("wanted error %v but got %v", tc.wantedError, err)
+				return
+			}
+			r.ServeHTTP(w, req)
 
-		if w.Code != tc.wantedCode {
-			t.Fatalf("wanted status code %d but got %d", tc.wantedCode, w.Code)
-			return
-		}
+			if w.Code != tc.wantedCode {
+				t.Fatalf("wanted status code %d but got %d", tc.wantedCode, w.Code)
+				return
+			}
+		})
 	}
 }
