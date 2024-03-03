47219acb8e5fcb0fcb380551161d9a8cf540a8b1
Xavi Aznar <xavier.aznar@seat.es>
2024-03-03 10:21:11 +0000

    refactor: remove Bash tests

--- tests/get_by_id_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -X PUT -d '{"Id": "01HQZ880A3N6Z6AZCJTF7JYPQH", "Name": "newThingy"}' \
-  -s http://localhost:8080/api/v1/thingy > /dev/null
-curl -s http://localhost:8080/api/v1/thingy/01HQZ880A3N6Z6AZCJTF7JYPQH
-
-echo
--- tests/post_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -X POST -s http://localhost:8080/api/v1/thingy/name/new-thingy
